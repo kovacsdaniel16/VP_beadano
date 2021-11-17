011a4f337b19bcb670aeaf3a9abbcb3632db4f6d
kovacsdaniel16 <kovacsdanieel@gmail.com>
2021-11-17 16:53:30 +0100

    Add project files.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Oltas;
+
+namespace VP_Beadando
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        cnOltas cn; //ez az adattag tárolja annak az objektumnak a referenciáját, ami az egész adatmodellt összefoglalja
+        public MainWindow()
+        {
+            InitializeComponent();
+            cn = new cnOltas();
+        }
+
+        private void ListView_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+    }
+}
